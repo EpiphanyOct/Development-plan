b5db57097378a93f754dc71827ced675063f5e50
unknown <unknown@example.com>
2023-10-30 14:35:55 +0800

    首页登录页的更新

--- miniprogram/pages/occupation/occupation.js
+++ miniprogram/pages/occupation/occupation.js
@@ -6,26 +6,61 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    items: [
+      {value: '0', name: '保存登录信息', checked: 'true'},
+      {value: '1', name: '不保存登录信息'}
+    ],
+    apple:'0'
   },
- open:function(event){
-   console.log(event.currentTarget.dataset.index)
-  wx.cloud.database().collection("login_users").add({
-    data:{
-      occupation:event.currentTarget.dataset.index,
-      _openid:app.globalData.openid
-    },
-    success(res){
-      app.globalData.occupation=event.currentTarget.dataset.index
-      wx.showToast({
-        title: '登录成功',
-      })
-      wx.reLaunch({
-        url: '/pages/user/user'
+
+
+
+
+
+
+  
+  save(res){
+    console.log(res)
+    this.data.apple = res.detail.value
+    //this.data.apple 
+  },
+
+
+
+
+  btnsub(res){
+    console.log(this.data.apple)
+    console.log(res)
+    console.log(res.detail.value.name)
+    wx.cloud.database().collection("login_users").where({
+      account:res.detail.value.name
+    }).get().then(ress=>{
+      console.log(ress)
     })
+    if(this.data.apple=="0"){
+    console.log("baocun")
+    wx.cloud.database().collection("login_users").add({
+      data:{
+        occupation:event.currentTarget.dataset.index,
+        _openid:app.globalData.openid
+      },
+      //success(res){
+        //app.globalData.occupation=event.currentTarget.dataset.index
+        //x.showToast({
+          //title: '登录成功',
+        //})
+        //wx.reLaunch({
+          //url: ''
+      //})
+      //}
+    })
+    }else{
+    console.log("bubaocun")
     }
-  })
- },
+    this.setData({
+      items: event.detail
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
